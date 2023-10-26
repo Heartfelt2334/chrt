d9662effd9876ba8cd2b7afdd0bf1a79e4479341
Heartfelt2334 <148968697+Heartfelt2334@users.noreply.github.com>
2023-10-26 16:37:51 +0500

    Delete ЧРТ — копия directory

--- ЧРТ — копия/assets/script/timetable-live-changer.js
+++
@@ -1,8 +0,0 @@
-fetch('https://chrtbackend.ravonzz.repl.co/api/timetable')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Ошибка:', error);
-  });
